45a1bbb289d81cb55b890c91b144107ffcbc8719
Pratham Gonate <Prathamgonte@proton.me>
2024-07-18 07:58:10 +0000

    luma changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const now = new Date();
+
+const ourTime = now.getHours()+":"+ now.getMinutes() +"PM IST ";
+
+console.log(ourTime);
+
+const timeEl = document.querySelector('.time')
+timeEl.textContent=ourTime;
