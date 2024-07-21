3318c73bf32fd2b4d53e2aff402720b68b7ff552
Pratham Gonate <Prathamgonte@proton.me>
2024-07-21 18:36:25 +0000

    luma website

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const now = new Date();
+const now = new Date()
 
-const ourTime = now.getHours()+":"+ now.getMinutes() +"PM IST ";
+const ourTime = `${now.getHours()}:${now.getMinutes()} PM IST`;
 
 console.log(ourTime);
 
 const timeEl = document.querySelector('.time')
-timeEl.textContent=ourTime;
+
+timeEl.textContent = ourTime;
